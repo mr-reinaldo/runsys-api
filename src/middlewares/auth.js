3855046a61714e5b2af6880ea0344ce577fe173e
Reinaldo Silva <josereinaldo.pessoal@gmail.com>
2023-02-21 20:51:41 -0300

    Remoção de códigos desnecessários, e adição de comentários para documentação do código.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,13 @@
 import jwt from 'jsonwebtoken';
 
+/**
+ * Middleware para verificar se o usuário está autenticado.
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {void}
+ * @throws {Error} Caso o token seja inválido.
+ */
 function isAuthenticated(req, res, next) {
     try {
         const { authorization } = req.headers;
@@ -8,8 +16,8 @@ function isAuthenticated(req, res, next) {
 
         const { uuid, email } = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.uuid = uuid;
-        req.email = email;
+        req.uuid = uuid; // Adiciona o uuid do usuário na requisição.
+        req.email = email; // Adiciona o email do usuário na requisição.
 
         next();
     } catch (error) {
